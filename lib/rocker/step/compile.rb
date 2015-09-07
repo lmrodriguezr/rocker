29f5ba7d8536d966f4e32c631f6e50da9cf7edcd
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-09-07 17:06:34 -0400

    Short for --table is now -T

--- lib/rocker/step/compile.rb
+++ lib/rocker/step/compile.rb
@@ -2,7 +2,7 @@
 # @author Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
 # @author Luis (Coto) Orellana
 # @license artistic license 2.0
-# @update Jun-05-2015
+# @update Sep-07-2015
 #
 
 class ROCker
@@ -14,17 +14,21 @@ class ROCker
       raise "-a/--alignment is mandatory." if @o[:aln].nil?
       raise "-a/--alignment must exist." unless File.exist? @o[:aln]
       if @o[:table].nil?
-	 raise "-t/--table is mandatory unless -b is provided." if @o[:blast].nil? or not File.exist? @o[:blast]
+	 raise "-T/--table is mandatory unless -b is provided." if
+	    @o[:blast].nil? or not File.exist? @o[:blast]
 	 @o[:table] = "#{@o[:blast]}.table"
       else
 	 @o[:reuse] = true
       end
-      raise "-b/--blast is mandatory unless -t exists." if @o[:blast].nil? and not File.exist? @o[:table]
+      raise "-b/--blast is mandatory unless -t exists." if
+	 @o[:blast].nil? and not File.exist? @o[:table]
       raise "-k/--rocker is mandatory." if @o[:rocker].nil?
 
       puts "Testing environment." unless @o[:q]
-      bash "echo '' | #{@o[:r]} --vanilla", "-r/--path-to-r must be executable. Is R installed?"
-      bash "echo \"library('pROC')\" | #{@o[:r]} --vanilla", "Please install the 'pROC' library for R first."
+      bash("echo '' | #{@o[:r]} --vanilla",
+	 "-r/--path-to-r must be executable. Is R installed?")
+      bash("echo \"library('pROC')\" | #{@o[:r]} --vanilla",
+	 "Please install the 'pROC' library for R first.")
 
       puts "Reading files." unless @o[:q]
       puts "  * loading alignment: #{@o[:aln]}." unless @o[:q]
@@ -44,7 +48,8 @@ class ROCker
       data.nucl = @o[:nucl]
       if @o[:refine]
 	 puts "  * refining windows." unless @o[:q]
-	 warn "Insufficient hits to refine results." unless data.refine! @o[:table]
+	 warn "Insufficient hits to refine results." unless
+	    data.refine! @o[:table]
       end
       puts "  * saving ROCker file: #{@o[:rocker]}." unless @o[:q]
       data.save @o[:rocker]
